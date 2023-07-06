361b63e3c6d3b6f5649a5df8d9997f216622650c
skm00147 <103888310+skm00147@users.noreply.github.com>
2023-07-06 11:49:18 +0900

    게임 종료 구현

--- the-cheese-game/Assets/C#script/EnergyBoard.cs
+++ the-cheese-game/Assets/C#script/EnergyBoard.cs
@@ -8,8 +8,15 @@ public class EnergyBoard : MonoBehaviour
     [SerializeField] private Slider slider;
     public float maxEnergy;
     private bool reset;
+    public GameObject PlayerObj;
+    public GameObject GameOverManu;
 
-    void Start()    //------------------------------------------------------------------------- 값 초기화
+    void Start()
+    {
+        ResetValue();
+    }
+
+    public void ResetValue()    //------------------------------------------------------------- 값 초기화
     {
         slider.maxValue = maxEnergy;    // 최대 에너지
         slider.value = maxEnergy;       // 현재 에너지
@@ -28,12 +35,13 @@ public class EnergyBoard : MonoBehaviour
             {
                 slider.value -= Time.deltaTime * 20;
             }
-            slider.value -= Time.deltaTime * 3;     // 초당 1만큼 에너지 감소
+            slider.value -= Time.deltaTime * 3;     // 초당 3만큼 에너지 감소
             reset = true;
         }
         else if(reset)                  // reset이 true일 때만 게임 종료 코드가 실행됨
         {
-            Debug.Log("You Die");               // 게임 종료 코드가 들어갈 자리
+            PlayerObj.SetActive(false);         // 게임 종료 코드
+            GameOverManu.SetActive(true);
             reset = false;                      // 더이상 게임 종료 코드가 실행되지 않도록
         }
     }
